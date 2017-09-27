b1852fe0c62d324043c0c312692017b74a83b9b3
bradgardiner <bradgardiner21@gmail.com>
2017-09-27 13:35:29 -0400

    Lab02 completed

---
+++ labs/lab-02/app/src/main/java/ca/uoit/csci4100u/lab_02/Lab02.java
@@ -0,0 +1,13 @@
+package ca.uoit.csci4100u.lab_02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lab02_part2);
+    }
+}
